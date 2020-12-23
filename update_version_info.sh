8a7af73fbc608dd48dc54d1b9d1ba7643ea171b5
hydazz <53986978+hydazz@users.noreply.github.com>
2020-12-23 14:10:24 +1100

    testing new workflow

---
+++ update_version_info.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OVERLAY_VERSION=$(curl -sX GET "https://raw.githubusercontent.com/hydazz/docker-baseimage-mono/main/version_info.json" | jq -r .overlay_version)
+MONO_VERSION=$(cat package_versions.txt | grep -E "mono-runtime.*?-" | sed -n 1p | cut -c 14- | sed -E 's/-r.*//g')
+OLD_OVERLAY_VERSION=$(cat version_info.json | jq -r .overlay_version)
+OLD_MONO_VERSION=$(cat version_info.json | jq -r .mono_version)
+OLD_SONARR_VERSION=$(cat version_info.json | jq -r .sonarr_version)
+
+sed -i \
+  -e "s/${OLD_OVERLAY_VERSION}/${OVERLAY_VERSION}/g" \
+  -e "s/${OLD_MONO_VERSION}/${MONO_VERSION}/g" \
+  -e "s/${OLD_SONARR_VERSION}/${SONARR_VERSION}/g" \
+  README.md
+
+NEW_VERSION_INFO="overlay_version|mono_version|sonarr_version
+${OVERLAY_VERSION}|${MONO_VERSION}|${SONARR_VERSION}"
+
+jq -Rn '
+( input  | split("|") ) as $keys |
+( inputs | split("|") ) as $vals |
+[[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
+' <<<"$NEW_VERSION_INFO" >version_info.json
