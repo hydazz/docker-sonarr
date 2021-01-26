b12f6cf6ce6911361415ed7ff172201511927b0b
hydazz <53986978+hydazz@users.noreply.github.com>
2021-01-26 20:07:25 +1100

    changed update wording

--- .github/update_readme.sh
+++ .github/update_readme.sh
@@ -46,7 +46,7 @@ See [package_versions.txt](package_versions.txt) for a full list of the packages
 
 ## Upgrading Sonarr
 
-To upgrade, all you have to do is pull our latest Docker image. We automatically check for Sonarr updates daily so there may be some delay when an update is released to when the image is updated.
+To upgrade, all you have to do is pull the latest Docker image. We automatically check for Sonarr updates daily so there may be some delay when an update is released to when the image is updated.
 
 ## Credits
 
